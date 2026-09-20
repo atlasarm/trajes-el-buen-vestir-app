4890ee705cd4bd9728177df9dd4ed27e0d9f413a
Nestor-Sarmiento <nesdav1005@gmail.com>
2026-09-19 23:43:40 -0500

    feat: integrar Swagger para documentación automática de la API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // <-- IMPORTACIONES CLAVE
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Facturación - Trajes El Buen Vestir')
+    .setDescription('Motor de facturación electrónica y comunicación con el SRI')
+    .setVersion('1.0')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
